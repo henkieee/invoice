9065bfbe236bd4ca8d415d58bf853ec15d7755a4
henk <henkiexxx4@gmail.com>
2015-11-20 17:05:58 +0100

    Added ui.router to the angular material tabs

---
+++ app/src/test/TestController.js
@@ -0,0 +1,75 @@
+(function(){
+
+  angular
+       .module('test')
+       .controller('TestController', [
+          'userService', '$mdSidenav', '$mdBottomSheet', '$log', '$q',
+         TestController
+       ]);
+
+  /**
+   * Main Controller for the Angular Material Starter App
+   * @param $scope
+   * @param $mdSidenav
+   * @param avatarsService
+   * @constructor
+   */
+  function TestController( testService, $mdSidenav, $mdBottomSheet, $log, $q) {
+    var self = this;
+
+    self.selected     = null;
+    self.users        = [ ];
+    self.selectUser   = selectUser;
+    self.toggleList   = toggleUsersList;
+    self.showContactOptions  = showContactOptions;
+
+    // Load all registered users
+
+    userService
+          .loadAllUsers()
+          .then( function( users ) {
+            self.users    = [].concat(users);
+            self.selected = users[0];
+          });
+
+    // *********************************
+    // Internal methods
+    // *********************************
+
+    /**
+     * Show the bottom sheet
+     */
+    function showContactOptions($event) {
+        var user = self.selected;
+
+        return $mdBottomSheet.show({
+          parent: angular.element(document.getElementById('content')),
+          templateUrl: './src/users/view/contactSheet.html',
+          controller: [ '$mdBottomSheet', ContactPanelController],
+          controllerAs: "cp",
+          bindToController : true,
+          targetEvent: $event
+        }).then(function(clickedItem) {
+          clickedItem && $log.debug( clickedItem.name + ' clicked!');
+        });
+
+        /**
+         * Bottom Sheet controller for the Avatar Actions
+         */
+        function ContactPanelController( $mdBottomSheet ) {
+          this.user = user;
+          this.actions = [
+            { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/svg/phone.svg'},
+            { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/svg/twitter.svg'},
+            { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/svg/google_plus.svg'},
+            { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/svg/hangouts.svg'}
+          ];
+          this.submitContact = function(action) {
+            $mdBottomSheet.hide(action);
+          };
+        }
+    }
+
+  }
+
+})();
